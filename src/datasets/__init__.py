62da178963277f88e563bca6777cd1106ec8cd96
Aurimas Racas <mail@aurimas.eu>
2024-11-17 18:32:57 -0500

    Refactor datasets

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -13,18 +13,29 @@ __all__ = ["sales"]
 class Sales:
     """Fake sales dataset."""
 
+    def get_url(self, gid: str) -> str:
+        """Get the URL of the dataset."""
+        return f"https://docs.google.com/spreadsheets/d/e/2PACX-1vTIvyGpgrsYw6aRXTjIavZEClY6UYxmepEpD1hQomCC-NPa7Th5qFQ0nD0LvEmrhzgK4F6mv4tnYTp-/pub?gid={gid}&single=true&output=csv"
+
     @cached_property
     def raw(self) -> pl.DataFrame:
         """Raw data."""
         return pl.read_csv(
-            "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIvyGpgrsYw6aRXTjIavZEClY6UYxmepEpD1hQomCC-NPa7Th5qFQ0nD0LvEmrhzgK4F6mv4tnYTp-/pub?gid=0&single=true&output=csv",
+            self.get_url("0"),
+        )
+
+    @cached_property
+    def aggregate_by_country_sku(self) -> pl.DataFrame:
+        """Aggregated data."""
+        return pl.read_csv(
+            self.get_url("1665578619"),
         )
 
     @cached_property
     def aggregate(self) -> pl.DataFrame:
         """Aggregated data."""
         return pl.read_csv(
-            "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIvyGpgrsYw6aRXTjIavZEClY6UYxmepEpD1hQomCC-NPa7Th5qFQ0nD0LvEmrhzgK4F6mv4tnYTp-/pub?gid=1665578619&single=true&output=csv",
+            self.get_url("405170366"),
         )
 
 
